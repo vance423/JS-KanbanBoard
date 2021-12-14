1becaf092774d6f09879f50606af25889e266820
vance <vancesoita2014@hotmail.com>
2021-12-14 17:33:54 +0300

    Completed module

--- js/kanban.js
+++ js/kanban.js
@@ -30,8 +30,24 @@ const create_item = () => {
   save_btn.innerHTML = 'Save';
   save_btn.addEventListener('click', () => {
     error.innerHTML = '';
+    if (input.value !=='') {
+      order += 1;
+      item.innerHTML = input.value;
+      adding = false;
+    } else {
+      error.innerHTML = message;
+    }
   });
+  item.appendChild(save_btn);
+
+  return item;
 };
 
 document.querySelectorAll('.drop').forEach(element => {
+  element.addEventListener('drop', event => {
+    event.preventDefault();
+    const id = event.dataTransfer.getData('text');
+    event.target.appendChild(document.getElementById(id));
+  });
+  element.addEventListener('dragover', event => event.preventDefault());
 });
