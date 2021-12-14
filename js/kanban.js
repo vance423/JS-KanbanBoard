e3f9f1e9989ac4e6188827ad5dae64e82892eb28
vance <vancesoita2014@hotmail.com>
2021-12-14 17:03:53 +0300

    Completed module

--- js/kanban.js
+++ js/kanban.js
@@ -16,7 +16,7 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
-  let item = document.createElement(div);
+  let item = document.createElement('div');
   item.classList.add('item');
   item.id = 'item-' + order;
   item.draggable = true;
